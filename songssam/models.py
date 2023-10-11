3fe2b6ccfa86c5fb9d5f0b76fd59809a2a14c441
ajou20658 <kwy1379@ajou.ac.kr>
2023-10-12 01:58:41 +0900

    model, serializer, views 수정

--- songssam/models.py
+++ songssam/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Song:
-    def __init__(self, file, output_dir, isUser, songId, userId, uuid):
+    def __init__(self, file ,isUser, uuid):
         self.file = file
         self.isUser = isUser
         self.uuid = uuid
