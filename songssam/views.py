9bc9dad47199210d045291f28e91259ffd9bbc66
ajou20658 <kwy1379@ajou.ac.kr>
2023-10-15 19:27:22 +0900

    fix

--- songssam/views.py
+++ songssam/views.py
@@ -291,7 +291,7 @@ def inference(request):
         output_file_path = str(uuid)+".wav"
         sf.write(output_file_path,waveT.T,sr,subtype = 'PCM_16',format='WAV')
         split_path = tmp_path+"/silent_noise"
-        FileCount = split_audio_silent(output_file_path,split_path)#음성 빈곳과 채워진 곳 분리
+        FileCount = split_audio_silent(split_path,output_file_path)#음성 빈곳과 채워진 곳 분리
         ##음성 빈 곳은 두고, 채워진 곳은 10초씩 분리하기, 파일이름 어떻게 해야되지
         ##파일 {No}_YES,{No}_No가 반복됨
         logger.info(FileCount)
