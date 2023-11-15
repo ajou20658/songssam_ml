195c3478dc20d712a9603ba33b6222ca2e7f3f9d
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-16 01:34:02 +0900

    views, preprocess

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('songssam/',include('songssam.urls')),
-    path('/',include('songssam.healthcheck'))
+    path('songssam/',include('songssam.urls'))
 ]
