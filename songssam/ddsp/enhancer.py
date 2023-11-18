b799d07adae94534bf45bc5ec8b345ac983383c0
Ubuntu <ubuntu@ip-172-31-21-72.us-east-2.compute.internal>
2023-11-18 19:53:03 +0000

    inference코드 추가

--- songssam/ddsp/enhancer.py
+++ songssam/ddsp/enhancer.py
@@ -1,8 +1,8 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
-from nsf_hifigan.nvSTFT import STFT
-from nsf_hifigan.models import load_model
+from .nsf_hifigan.nvSTFT import STFT
+from .nsf_hifigan.models import load_model
 from torchaudio.transforms import Resample
 
 class Enhancer:
