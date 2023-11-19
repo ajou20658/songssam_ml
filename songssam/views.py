1d58ecf4c7102a396e180faf41809fc983f147a0
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-19 17:33:00 +0900

    serializer 수정

--- songssam/views.py
+++ songssam/views.py
@@ -373,7 +373,106 @@ def inference(request):
         logger.error(error_message)
         # send_post_request([],848,uuid)
         return JsonResponse({"error":"error"},status = 411)
+    
+class SvcDDSP:
+    def __init__(self, model_path, vocoder_based_enhancer, enhancer_adaptive_key, input_pitch_extractor,
+                 f0_min, f0_max, threhold, spk_id, spk_mix_dict, enable_spk_id_cover):
+        self.model_path = model_path
+        self.vocoder_based_enhancer = vocoder_based_enhancer
+        self.enhancer_adaptive_key = enhancer_adaptive_key
+        self.input_pitch_extractor = input_pitch_extractor
+        self.f0_min = f0_min
+        self.f0_max = f0_max
+        self.threhold = threhold
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.spk_id = spk_id
+        self.spk_mix_dict = spk_mix_dict
+        self.enable_spk_id_cover = enable_spk_id_cover
+        
+        # load ddsp model
+        self.model, self.args = load_model(self.model_path, device=self.device)
+        
+        # load units encoder
+        if self.args.data.encoder == 'cnhubertsoftfish':
+            cnhubertsoft_gate = self.args.data.cnhubertsoft_gate
+        else:
+            cnhubertsoft_gate = 10
+        self.units_encoder = Units_Encoder(
+            self.args.data.encoder,
+            self.args.data.encoder_ckpt,
+            self.args.data.encoder_sample_rate,
+            self.args.data.encoder_hop_size,
+            cnhubertsoft_gate=cnhubertsoft_gate,
+            device=self.device)
+        
+        # load enhancer
+        if self.vocoder_based_enhancer:
+            self.enhancer = Enhancer(self.args.enhancer.type, self.args.enhancer.ckpt, device=self.device)
 
+    def infer(self, input_wav, pitch_adjust, speaker_id, safe_prefix_pad_length):
+        print("Infer!")
+        # load input
+        audio, sample_rate = librosa.load(input_wav, sr=None, mono=True)
+        if len(audio.shape) > 1:
+            audio = librosa.to_mono(audio)
+        hop_size = self.args.data.block_size * sample_rate / self.args.data.sampling_rate
+        
+        # safe front silence
+        if safe_prefix_pad_length > 0.03:
+            silence_front = safe_prefix_pad_length - 0.03
+        else:
+            silence_front = 0
+            
+        # extract f0
+        pitch_extractor = F0_Extractor(
+            self.input_pitch_extractor,
+            sample_rate,
+            hop_size,
+            float(self.f0_min),
+            float(self.f0_max))
+        f0 = pitch_extractor.extract(audio, uv_interp=True, device=self.device, silence_front=silence_front)
+        f0 = torch.from_numpy(f0).float().to(self.device).unsqueeze(-1).unsqueeze(0)
+        f0 = f0 * 2 ** (float(pitch_adjust) / 12)
+        
+        # extract volume
+        volume_extractor = Volume_Extractor(hop_size)
+        volume = volume_extractor.extract(audio)
+        mask = (volume > 10 ** (float(self.threhold) / 20)).astype('float')
+        mask = np.pad(mask, (4, 4), constant_values=(mask[0], mask[-1]))
+        mask = np.array([np.max(mask[n : n + 9]) for n in range(len(mask) - 8)])
+        mask = torch.from_numpy(mask).float().to(self.device).unsqueeze(-1).unsqueeze(0)
+        mask = upsample(mask, self.args.data.block_size).squeeze(-1)
+        volume = torch.from_numpy(volume).float().to(self.device).unsqueeze(-1).unsqueeze(0)
+
+        # extract units
+        audio_t = torch.from_numpy(audio).float().unsqueeze(0).to(self.device)
+        units = self.units_encoder.encode(audio_t, sample_rate, hop_size)
+        
+        # spk_id or spk_mix_dict
+        if self.enable_spk_id_cover:
+            spk_id = self.spk_id
+        else:
+            spk_id = speaker_id
+        spk_id = torch.LongTensor(np.array([[spk_id]])).to(self.device)
+        
+        # forward and return the output
+        with torch.no_grad():
+            output, _, (s_h, s_n) = self.model(units, f0, volume, spk_id = spk_id, spk_mix_dict = self.spk_mix_dict)
+            output *= mask
+            if self.vocoder_based_enhancer:
+                output, output_sample_rate = self.enhancer.enhance(
+                                                                output, 
+                                                                self.args.data.sampling_rate, 
+                                                                f0, 
+                                                                self.args.data.block_size,
+                                                                adaptive_key = self.enhancer_adaptive_key,
+                                                                silence_front = silence_front)
+            else:
+                output_sample_rate = self.args.data.sampling_rate
+
+            output = output.squeeze().cpu().numpy()
+            return output, output_sample_rate
+        
 use_vocoder_based_enhancer = True
 
 enhancer_adaptive_key = 0
@@ -395,7 +494,7 @@ spk_mix_dict = None
 @api_view(['GET'])
 def voice_change_model(request):
     # 요청에서 쿼리 파라미터를 추출하여 InferSerializer로 변환
-    serializer = InferSerializer(data=request.query_params)
+    serializer = InferSerializer(data=request.GET)
 
     # serializer가 유효한지 확인
     if serializer.is_valid():
@@ -404,7 +503,7 @@ def voice_change_model(request):
         uuid = serializer.validated_data["uuid"]
     else:
         logger.info("serializer 오류")
-        return JsonResponse({"error":"error"},status=404)
+        return HttpResponse({"error":"error"},status=404)
     logger.info(f_wave_path)
     logger.info(f_ptr_path)
     # 결과를 저장할 경로 생성
@@ -547,106 +646,6 @@ def voice_change_model(request):
     # response['Content-Disposition'] = 'attachment; filename="audio.mp3"'  # 파일을 다운로드할 수 있도록 설정
 
     return response
-
-
-class SvcDDSP:
-    def __init__(self, model_path, vocoder_based_enhancer, enhancer_adaptive_key, input_pitch_extractor,
-                 f0_min, f0_max, threhold, spk_id, spk_mix_dict, enable_spk_id_cover):
-        self.model_path = model_path
-        self.vocoder_based_enhancer = vocoder_based_enhancer
-        self.enhancer_adaptive_key = enhancer_adaptive_key
-        self.input_pitch_extractor = input_pitch_extractor
-        self.f0_min = f0_min
-        self.f0_max = f0_max
-        self.threhold = threhold
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.spk_id = spk_id
-        self.spk_mix_dict = spk_mix_dict
-        self.enable_spk_id_cover = enable_spk_id_cover
-        
-        # load ddsp model
-        self.model, self.args = load_model(self.model_path, device=self.device)
-        
-        # load units encoder
-        if self.args.data.encoder == 'cnhubertsoftfish':
-            cnhubertsoft_gate = self.args.data.cnhubertsoft_gate
-        else:
-            cnhubertsoft_gate = 10
-        self.units_encoder = Units_Encoder(
-            self.args.data.encoder,
-            self.args.data.encoder_ckpt,
-            self.args.data.encoder_sample_rate,
-            self.args.data.encoder_hop_size,
-            cnhubertsoft_gate=cnhubertsoft_gate,
-            device=self.device)
-        
-        # load enhancer
-        if self.vocoder_based_enhancer:
-            self.enhancer = Enhancer(self.args.enhancer.type, self.args.enhancer.ckpt, device=self.device)
-
-    def infer(self, input_wav, pitch_adjust, speaker_id, safe_prefix_pad_length):
-        print("Infer!")
-        # load input
-        audio, sample_rate = librosa.load(input_wav, sr=None, mono=True)
-        if len(audio.shape) > 1:
-            audio = librosa.to_mono(audio)
-        hop_size = self.args.data.block_size * sample_rate / self.args.data.sampling_rate
-        
-        # safe front silence
-        if safe_prefix_pad_length > 0.03:
-            silence_front = safe_prefix_pad_length - 0.03
-        else:
-            silence_front = 0
-            
-        # extract f0
-        pitch_extractor = F0_Extractor(
-            self.input_pitch_extractor,
-            sample_rate,
-            hop_size,
-            float(self.f0_min),
-            float(self.f0_max))
-        f0 = pitch_extractor.extract(audio, uv_interp=True, device=self.device, silence_front=silence_front)
-        f0 = torch.from_numpy(f0).float().to(self.device).unsqueeze(-1).unsqueeze(0)
-        f0 = f0 * 2 ** (float(pitch_adjust) / 12)
-        
-        # extract volume
-        volume_extractor = Volume_Extractor(hop_size)
-        volume = volume_extractor.extract(audio)
-        mask = (volume > 10 ** (float(self.threhold) / 20)).astype('float')
-        mask = np.pad(mask, (4, 4), constant_values=(mask[0], mask[-1]))
-        mask = np.array([np.max(mask[n : n + 9]) for n in range(len(mask) - 8)])
-        mask = torch.from_numpy(mask).float().to(self.device).unsqueeze(-1).unsqueeze(0)
-        mask = upsample(mask, self.args.data.block_size).squeeze(-1)
-        volume = torch.from_numpy(volume).float().to(self.device).unsqueeze(-1).unsqueeze(0)
-
-        # extract units
-        audio_t = torch.from_numpy(audio).float().unsqueeze(0).to(self.device)
-        units = self.units_encoder.encode(audio_t, sample_rate, hop_size)
-        
-        # spk_id or spk_mix_dict
-        if self.enable_spk_id_cover:
-            spk_id = self.spk_id
-        else:
-            spk_id = speaker_id
-        spk_id = torch.LongTensor(np.array([[spk_id]])).to(self.device)
-        
-        # forward and return the output
-        with torch.no_grad():
-            output, _, (s_h, s_n) = self.model(units, f0, volume, spk_id = spk_id, spk_mix_dict = self.spk_mix_dict)
-            output *= mask
-            if self.vocoder_based_enhancer:
-                output, output_sample_rate = self.enhancer.enhance(
-                                                                output, 
-                                                                self.args.data.sampling_rate, 
-                                                                f0, 
-                                                                self.args.data.block_size,
-                                                                adaptive_key = self.enhancer_adaptive_key,
-                                                                silence_front = silence_front)
-            else:
-                output_sample_rate = self.args.data.sampling_rate
-
-            output = output.squeeze().cpu().numpy()
-            return output, output_sample_rate
  
 
 def filter(filepath,threshold):
