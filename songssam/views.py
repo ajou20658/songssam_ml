2fd325b8c3a5d5db7e1b271f9f20e2f0b2023548
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-20 00:06:14 +0900

    복구1

--- songssam/views.py
+++ songssam/views.py
@@ -621,11 +621,11 @@ def voice_change_model(request):
     # sf.write(out_wav_path, tar_audio, daw_sample, format="wav")
     # mp3 = AudioSegment.from_file(out_wav_path,format="wav")
 
-    y1,sample_rate1=librosa.load(MR_file_path,mono=True)
+    y1,sample_rate1=librosa.load(MR_file_path,mono=True,sr=daw_sample)
     min_len=min(len(y1),len(tar_audio))
     y1 = y1[:min_len]
     tar_audio = tar_audio[:min_len]
-    audio = AudioSegment(tar_audio.tobytes(),frame_rate = daw_sample,sample_width=tar_audio.dtype.itemsize,channels=len(tar_audio.shape))
+    audio = AudioSegment(y1.tobytes(),frame_rate = daw_sample,sample_width=tar_audio.dtype.itemsize,channels=len(tar_audio.shape))
     audio_bytes = audio.export(format='mp3').read()
     
     response = HttpResponse(content=audio_bytes, content_type='audio/mpeg')
